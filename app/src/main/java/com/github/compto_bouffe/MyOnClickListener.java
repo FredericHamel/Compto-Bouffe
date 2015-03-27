22ab9eb6277f0a40813d03f8ccd45ed0a1c7039c
Gainrrom <sricher10@gmail.com>
2015-03-27 18:33:41 -0400

    Contenu:
    -MainActivity = FicheA (laissons le nom "MainActivity" intact étant donné
    qu'il s'agit de la fiche de base)
    
    -FicheB
        -bouton "AUJOURD'HUI" qui permet d'aller à FicheC quand on clique dessus)
    
    -FicheC

--- app/src/main/java/com/github/compto_bouffe/MyOnClickListener.java
+++ app/src/main/java/com/github/compto_bouffe/MyOnClickListener.java
@@ -5,7 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 
 /**
- * Created by sr on 2015-03-26.
+ * Created by sr on 2015-03-26.//
  */
 
 public class MyOnClickListener extends ActionBarActivity implements View.OnClickListener{
