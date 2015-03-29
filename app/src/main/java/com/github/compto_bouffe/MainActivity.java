a7bded11f38d0a364021fd8426b72cad1ebf620c
Frederic Hamel <frederic.hamel123@gmail.com>
2015-03-29 12:33:46 -0400

    AndroidManifest.xml:   Modifier les proprietes de l'activite principal dans le manifeste.
    MainActivity.java:
      Permet choisir l'activiter de depart
    dependement si les SharedPreference sont
    initialiser ou nom.
    
    Enlever le layout du MainActivity, car il n'en a
    pas besoin il ne fait que demarrer une activite.

--- app/src/main/java/com/github/compto_bouffe/MainActivity.java
+++ app/src/main/java/com/github/compto_bouffe/MainActivity.java
@@ -1,39 +1,26 @@
 package com.github.compto_bouffe;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        SharedPreferences sharePrefs = getSharedPreferences("profile", Context.MODE_PRIVATE);
+        String name = sharePrefs.getString("name", "");
+        Class<?> obj;
+        if(name.equals(""))
+            obj = FicheB.class; //FicheA.class;
+        else
+            obj = FicheB.class;
+        Log.d(MainActivity.class.getName(), "Start " + obj.getName());
+        startActivity(new Intent(this, obj));
+        finish();
     }
 }
