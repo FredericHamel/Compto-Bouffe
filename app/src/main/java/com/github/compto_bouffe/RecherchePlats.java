e1f57e3ca468e401bbb462b6d487437b1a6d7f5a
Frederic Hamel <frederic.hamel123@gmail.com>
2015-04-07 16:32:03 -0400

    Fix les conflit.

--- app/src/main/java/com/github/compto_bouffe/RecherchePlats.java
+++ app/src/main/java/com/github/compto_bouffe/RecherchePlats.java
@@ -1,23 +1,44 @@
 package com.github.compto_bouffe;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.github.compto_bouffe.api.LabelAPI;
+import com.github.compto_bouffe.api.Product;
+
+import java.util.ArrayList;
 
 public class RecherchePlats extends Activity {
 
     private TextView queryText;
     private Button sendQueryBtn;
+    private ArrayList<Product> products;
+    private LabelAPI labelAPI;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recherche_plats);
+        labelAPI = LabelAPI.getInstance();
         // get queryText
         queryText = (TextView)findViewById(R.id.query_text);
         sendQueryBtn = (Button)findViewById(R.id.send_request);
+
+        sendQueryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = queryText.getText().toString();
+                if(!text.equals("")) {
+
+                }
+            }
+        });
     }
 
 
@@ -41,5 +62,23 @@ public class RecherchePlats extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class SearchProduct extends AsyncTask<String, Integer, Long>
+    {
+        @Override
+        protected Long doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Long aLong) {
+            super.onPostExecute(aLong);
+        }
     }
 }
